d9a1fc833494a35c5c7ef9cd5d77e0b56f546b5c
Halima Abedin <halimaabedin@gmail.com>
2019-05-08 19:34:03 -0400

    This is my first commit

---
+++ src/main/java/com/TalenTech/mavenfromcmd/App.java
@@ -0,0 +1,13 @@
+package com.TalenTech.mavenfromcmd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
